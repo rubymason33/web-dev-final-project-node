9276014e96dbf556b54d0004f2fc859d80d04365
Ruby Mason <mason.r@northeastern.edu>
2025-06-17 11:40:27 -0400

    setup

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import mongoose from "mongoose";
+import cors from "cors";
+import "dotenv/config";
+import session from "express-session";
+
+// const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+// mongoose.connect(CONNECTION_STRING);
+
+const app = express();
+
+// configure for branch deploys
+const allowedOrigins = [
+    process.env.NETLIFY_URL,
+];
+
+app.use(cors({
+    credentials: true,
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+        } else {
+        return callback(new Error("Not allowed by CORS"));
+        }
+    }
+}));
+
+// app.use(cors({
+//     credentials: true,
+//     origin: process.env.NETLIFY_URL || "http://localhost:5173",
+//     })
+// );
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
+app.use(express.json());
+app.listen(process.env.PORT || 4000);
